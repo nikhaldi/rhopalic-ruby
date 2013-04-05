3eb6d5429b331c8e01c77aecccbd87daaa106e6f
Nik Haldimann <nhaldimann@gmail.com>
2013-04-05 17:41:24 -0400

    Basic implementation, with many defects to be fixed

--- lib/rhopalic/rhopalic.rb
+++ lib/rhopalic/rhopalic.rb
@@ -1,7 +1,25 @@
+require 'lingua'
+
 class Rhopalic
 
   def self.is_rhopalic(phrase)
-    true
+    # TODO handle apostrophes
+    last_letter_count = 0
+    last_syllable_count = 0
+    phrase.scan(/\w+/) do |match|
+      letter_count = match.length
+      syllable_count = Lingua::EN::Syllable.syllables(match)
+
+      if last_letter_count > 0 && last_letter_count != letter_count - 1 &&
+          last_syllable_count > 0 && last_syllable_count != syllable_count - 1
+        return false
+      end
+
+      last_letter_count = letter_count
+      last_syllable_count = syllable_count
+    end
+
+    return true
   end
 
 end
--- test/rhopalic_test.rb
+++ test/rhopalic_test.rb
@@ -1,3 +1,5 @@
+# encoding: UTF-8
+
 require 'param_test'
 require 'test/unit'
 
@@ -5,8 +7,30 @@ require 'rhopalic'
 
 class RhopalicTest < ActiveSupport::TestCase
 
-  param_test "phrase %s is rhopalic",
-  ["a be"] do |phrase|
+  param_test "phrase %s is rhopalic", [
+    "a",
+    "a be",
+    "the fine horse",
+    "Words along rhopalic pentameters",
+    # TODO these don't work. pull in dictionary?
+    #"Add extra syllables gradually",
+    #"While shadows, lengthening, attenuate"
+    "Lines thicken approaching termination.",
+  ] do |phrase|
     assert Rhopalic.is_rhopalic(phrase)
   end
+
+  param_test "phrase %s is not rhopalic", [
+    "a b",
+    "be do",
+    "a be ce",
+  ] do |phrase|
+    assert !Rhopalic.is_rhopalic(phrase)
+  end
+
+  # TODO doesn't work because of word definition
+  # param_test "phrase %s with accented characters is rhopalic",
+  # ["the café"] do |phrase|
+  #   assert Rhopalic.is_rhopalic(phrase)
+  # end
 end
